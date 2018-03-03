e032c868ae902f993a176d5af2e82492ec489aa3
Eric Hollas <eric.hollas@gmail.com>
2018-03-02 23:49:26 -0600

    Implemented Verb

--- option.go
+++ option.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"container/list"
 	"fmt"
 )
 
-type optionCB func(option) bool
+type optionCB func(string) bool
 
 type option struct {
 	text        string
@@ -25,7 +26,43 @@ func newOptionCB(text, description string, callback optionCB) *option {
 	return &option{text, description, callback, ""}
 }
 
+type verb struct {
+	main       *option
+	suboptions *list.List
+	callback   optionCB
+}
+
+func newVerb(text, description string, callback optionCB) *verb {
+	v := verb{}
+	v.main = newOption(text, description)
+	v.callback = callback
+	v.suboptions = list.New()
+	return &v
+}
+
+func (v verb) onVerbFound(val string) {
+	if v.callback != nil {
+		v.callback(val)
+	}
+}
+
+func (v verb) String() string {
+	s := fmt.Sprintf("%-5s %s", v.main.text, v.main.description)
+	for e := v.suboptions.Front(); e != nil; e = e.Next() {
+		s = s + fmt.Sprintf("\n     -%s", e.Value)
+	}
+	return s
+}
+
+func onVerbGet(val string) bool {
+	fmt.Println("Get verb here saw " + val)
+	return true
+}
+
 func main() {
-	opt := newOption("test", "Verb to run a test")
-	fmt.Println(opt)
+	opt := newOption("test", "Select the test option")
+	verb := newVerb("get", "Retrieve information about the node", onVerbGet)
+	verb.suboptions.PushBack(opt)
+	fmt.Println(verb)
+	verb.onVerbFound("Bitching")
 }
